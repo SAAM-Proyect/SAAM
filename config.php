9c790deb29dcaae7b00e78f02cea28b8c4fdcdfb
Horacio Darinel Espinosa Barceló <horacioespinosa@lifeprogramming.net>
2015-10-27 01:15:54 -0500

    Pequeños cambios

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
     define('URL', 'http://localhost/SAAM/');
     define('LIBS','libs/');
+
     define('DB_TYPE','mysql');
     define('DB_HOST' , 'localhost');
     define('DB_USER' , 'root' );
     define('DB_PASS' , '' );
     define('DB_NAME' , 'SAAM');
+
     define('ALGO', 'sha512');
     define('HASH_KEY', 'supermaneselmejor@');
