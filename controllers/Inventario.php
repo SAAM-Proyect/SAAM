b57ac382992d51ece869cc494070c68df25767ce
Horacio Darinel Espinosa Barceló <horacioespinosa@lifeprogramming.net>
2015-10-30 18:12:19 -0500

    alskdjasd

--- controllers/Inventario.php
+++ controllers/Inventario.php
@@ -3,6 +3,13 @@
         function __construct(){
             parent:: __construct();
         }
+        function index(){
+            if(Session::exist()){
+                header("Location: ".URL."/Inventario/inventario");
+            }else{
+                header("Location: ".URL);
+            }
+        }
         function _getProduct(){
             $data["codProducto"] = $_POST["codProducto"];
             return $this->model->_getProduct($data);
