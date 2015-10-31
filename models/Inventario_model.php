2a5354ad045aa8e91fbcdc8fd56b8e32de0398e1
muppet3 <rockfer74@hotmail.com>
2015-10-31 17:40:54 -0600

    xD

--- models/Inventario_model.php
+++ models/Inventario_model.php
@@ -1,9 +1,9 @@
 <?php
     class Inventario_model extends Model{
-        function __construct(){
-            parent:: __construct();
-        }
-        function _getProduct($data){
-            return $this->db->procedure_S("Select_Producto",$data);
-        }
+    function __construct(){
+        parent:: __construct();
     }
+    function _getProduct($data){
+        return $this->db->procedure_S("Select_Producto",$data);
+    }
+}
