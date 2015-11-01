4ec5a00604bc51c257d4409aef6ee975a4f12a8e
IrvinEscalante <shadow_punk16@hotmail.com>
2015-10-31 20:24:28 -0500

    agregar el index de tareas

---
+++ views/tareas/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dark_HaseO
+ * Date: 27/10/2015
+ * Time: 11:12 PM
+ */
+require('views/header.php'); ?>
+
+<html>
+<head>
+</head>
+<body>
+<script type="text/javascript">
+    document.write("<div class = cont>")
+    for (var i=0;i<=4;i++){
+        document.write("<div class='yolo'>")
+            document.write("<div class='icon'>")
+                document.write("<div class='imag'>")
+                document.write("</div>");
+            document.write("</div>")
+        document.write("</div>")
+    }
+    document.write("</div>")
+</script>
+</body>
+</html>
+
+
+
+
+
+<?php require('views/footer.php'); ?>
