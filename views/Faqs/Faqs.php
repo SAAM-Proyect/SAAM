dd4f8f86619fd940f46dc2466125619910c90064
muppet3 <rockfer74@hotmail.com>
2015-10-31 19:22:13 -0600

    xD

--- views/Faqs/Faqs.php
+++ views/Faqs/Faqs.php
@@ -4,15 +4,12 @@
     <title>FAQS</title>
 </head>
 <body>
-
-
 <form method="post" action="">
     <div>   <input type="text" name="Asunto" placeholder="Asunto"/> </div>  </br>
     <div>
         </br>
         <input type="text" name="Pregunta" placeholder="Comentario" />
     </div>
-
     <input type="submit" name="Enviar" value="Comentar"/>
 </form>
 </body>
