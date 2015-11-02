481fee1c7dda934ac15f5d49a2232741caa2f294
IrvinEscalante <shadow_punk16@hotmail.com>
2015-11-02 16:27:12 -0500

    cambios irvin2

--- views/tareas/index.php
+++ views/tareas/index.php
@@ -12,14 +12,19 @@ require('views/header.php'); ?>
 </head>
 <body>
 <script type="text/javascript">
-    document.write("<div class = cont>")
-    for (var i=0;i<=4;i++){
-        document.write("<div class='yolo'>")
+    var posiciones = 6;
+    document.write("<div class='inicial'><div class='titulo'>oansdoandko</div></div>")
+    document.write("<div class='cont'>")
+    for (var i=1;i<=posiciones;i++){
+        document.write("<div class='posicion'>")
             document.write("<div class='icon'>")
                 document.write("<div class='imag'>")
                 document.write("</div>")
             document.write("</div>")
         document.write("</div>")
+        if (i == posiciones){
+            document.write("<div class='relleno'></div>")
+        }
     }
     document.write("</div>")
 </script>
