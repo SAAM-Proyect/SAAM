1a70e9e1c1a597206806dc1db2b74c29646bb32b
muppet3 <rockfer74@hotmail.com>
2015-11-02 17:41:03 -0600

    xD

--- views/Faqs/Faqs.php
+++ views/Faqs/Faqs.php
@@ -5,16 +5,19 @@
     <title>FAQS</title>
 </head>
 <body>
-<form method="post" action="">
-    <div>   <input type="text" name="Asunto" placeholder="Asunto"/> </div>  </br>
-    <div>
-        </br>
-        <textarea type="text" name="Pregunta" placeholder="Comentario" ></textarea>
-    </div>
-    <input type="submit" name="Enviar" value="Comentar"/>
+<form name="faqs_form">
+    <input type="text" name="Asunto" placeholder="Asunto"/>
+    <textarea type="text" name="Pregunta" placeholder="Comentario" ></textarea>
+    <textarea type="text" name="Respuesta" placeholder="Respuesta" ></textarea>
+    <input id="crear" type="submit" name="Enviar" value="Comentar"/>
 </form>
+<div class="mensajex"></div>
+<div id="mycontainer"></div>
 </body>
 </html>
+
+<?php require("views/footer.php"); ?>
+
 
 
 
