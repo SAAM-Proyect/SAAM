35e1b3f8287b210b08fd2aabfe2a02ab40de0b98
MoisesBarrera <moises.b.k@hotmail.com>
2015-11-05 22:17:21 -0500

    Merge branch 'master' of /Applications/MAMP/htdocs/SAAM with conflicts.

--- views/header.php
+++ views/header.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Adjuntamos los metadatos -->
+    <!--Adjuntamos los metadatos-->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="msapplication-tap-highlight" content="no">
-
     <title>SAAM</title>
-
-    <!-- Adjuntamos los CSS -->
     <link rel="icon" type="image/png" href="<?php echo URL; ?>public/img/favicon.png"/>
+    <!-- Compiled and minified CSS -->
+    <!--link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css"/-->
     <link href="<?php echo URL; ?>public/css/materialize.css" rel="stylesheet" type="text/css"/>
-    <link  href="<?php echo URL; ?>public/css/style.css" rel="stylesheet"/>
-    <link  href="<?php echo URL; ?>public/css/ourstyles.css" rel="stylesheet"/>
-    <link href="<?php echo URL; ?>public/css/styleslogin.css" rel="stylesheet"/>
-
-    <!-- Adjuntamos los Javascript -->
+    <!-- CSS de Omar-->
+    <link href="<?php echo URL; ?>public/css/styleInventario.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>public/css/fontsInventarios.css">
+    <!-- ----- -->
+    <link lazyload href="<?php echo URL; ?>public/css/style.css" rel="stylesheet"/>
+    <link lazyload href="<?php echo URL; ?>public/css/estilo.css" rel="stylesheet"">
+    <!-- Compiled and minified JavaScript -->
     <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.min.js"></script>
     <script src="<?php echo URL; ?>public/js/materialize/js/bin/materialize.min.js" defer></script>
     <script async="true" src="<?php echo URL; ?>public/js/script.js" type="text/javascript"></script>
     <script async="true" src="<?php echo URL; ?>public/js/_login.js" type="text/javascript"></script>
     <script async="true" src="<?php echo URL; ?>public/js/faqs.js" type="text/javascript"></script>
-    <script type="text/javascript" src="<?php echo URL; ?>public/js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>public/css/Faqs.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URL;?>public/css/estilosMoises.css">
+    <link  href="<?php echo URL; ?>public/css/tareasestilos.css" rel="stylesheet"/>
+    <link  href="<?php echo URL; ?>public/css/menu.css" rel="stylesheet"/>
     <script>
         $(function(){
             $('#id').addClass('hover').removeClass('normal').fadeIn("slow");
@@ -48,6 +52,9 @@
             });
         })(jQuery);
     </script>
+    <script type="text/javascript" src="<?php echo URL; ?>public/js/script.js"></script>
+    <link  href="<?php echo URL; ?>public/css/estilosMoises.css" rel="stylesheet"/>
+    <link  href="<?php echo URL; ?>public/css/estilo.css" rel="stylesheet"/>
 </head>
 <body>
 <nav id='cssmenu'>
@@ -56,13 +63,19 @@
         <li><a href='#'><span>Clientes</span></a></li>
         <li><a href='#'><span>Archivos</span></a></li>
         <li><a href='<?php echo URL."Tareas"; ?>'><span>Tareas</span></a></li>
-        <li><a href='<?php echo URL."Proyectos"; ?>'><span>Proyectos</span></a></li>
-        <li><a href="<?php echo URL."Inventario/inventario";?>"><span>Inventario</span></a></li>
-        <li><a href="<?php echo URL."Faqs/Faqs";?>"><span>Faqs</span></a></li>
+        <li class="has-sub">
+            <a href='<?php echo URL."Proyectos"; ?>'><span>Proyectos</span></a>
+            <ul>
+                <li><a href="<?php echo URL."Proyectos/alta"; ?>">Alta</a></li>
+                <li><a href="<?php echo URL."Proyectos/modificaciones"; ?>">Modificaciones</a></li>
+            </ul>
+        </li>
         <li class="has-sub"><a href=''><span><?php echo ucfirst(Session::getValue('U_NAME')); ?></span></a>
             <ul>
                 <li><a href="<?php echo URL."Usuario/perfil"; ?>">Ver perfil</a></li>
                 <li><a href="#">Configuración</a></li>
+                <li><a href="<?php echo URL."Inventario/inventario";?>">Inventario</a></li>
+                <li><a href="<?php echo URL."Proyecto";?>">Inventario</a></li>
                 <li><a href="<?php echo URL."Usuario/salir"; ?>">Salir</a></li>
             </ul>
         </li>
