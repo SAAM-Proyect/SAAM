3ad3c725a599a4b4f602e163129c91d825a6cd6a
IrvinEscalante <shadow_punk16@hotmail.com>
2015-11-12 17:39:52 -0500

    cambios  irvin

--- views/tareas/index.php
+++ views/tareas/index.php
@@ -7,24 +7,81 @@
  */
 require('views/header.php'); ?>
 
-<script type="text/javascript">
-    var posiciones = 4;
-    var nombre_Pos= 'irvin_pro';
-    document.write("<div class='inicial'><div class='text'>Tareas</div></div>")
-    document.write("<div class='cont'>")
-    for (var i=1;i<=posiciones;i++){
-        document.write("<div class='posicion'>")
-            document.write("<div class='icon'>")
-                document.write("<div class='imag'>")
-                document.write("</div>")
-            document.write("</div>")
-        document.write("<div class='textpos'>" + nombre_Pos + "</div>")
-        document.write("</div>")
-        if (i == posiciones){
-            document.write("<div class='relleno'></div>")
-        }
+<div class='inicial column'>
+    <div class='text'>
+        Tareas
+    </div>
+    <div class='Form_tarea'>
+            <div class='Estado_Proyecto A_Detalles'>
+                    <div class='progress-radial progress-65'>
+                            <div class='overlay'>65%</div>
+                        </div>
+                    <div class='card-title' style='font-size: 20px !important;'>Titulo Proyecto</div>
+                    <div >hola mundo</div>
+                    <div >hola mundo</div>
+                </div>
+        </div>
+</div>
+
+
+<?php
+$posiciones = 2;
+$nombre_Pos= 'irvin_pro';
+/*          fin de importado de moi */
+echo "<div class='cont'>";
+for ($i=1;$i<=$posiciones;$i++){
+    echo "<div class='posicion column'>";
+    echo "<div class='icon'>";
+    echo "<div class='imag'>";
+    echo "</div>";
+    echo "</div>";
+    echo "<div class='textpos'>".$nombre_Pos. "</div>";
+    $num_tareas = 2;
+
+
+    for ($k = 1;$k<=$num_tareas; $k++) {
+        echo "<div class='Form_tarea'>";
+        echo "    <div class='Estado_Proyecto A_Detalles'>";
+        echo "        <div class='progress-radial progress-65'>";
+        echo "            <div class='overlay'>65%</div>";
+        echo "        </div>";
+        echo "        <div class='card-title' style='font-size: 20px !important;'>Titulo Proyecto</div>";
+        echo "        <div >hola mundo</div>";
+        echo "        <div >hola mundo</div>";
+        echo "    </div>";
+        echo "</div>";
     }
-    document.write("</div>")
+
+    echo "</div>";
+
+    if (i == posiciones){
+        echo "<div class='relleno'></div>";
+    }
+}
+echo "</div>"
+?>
+
+<script>
+    $(function() {
+        $( ".column" ).sortable({
+            connectWith: ".posicion",
+            handle: ".Estado_Proyecto",
+            cancel: ".portlet-toggle",
+            placeholder: "portlet-placeholder ui-corner-all"
+        });
+
+        $( ".portlet" )
+            .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+            .find( ".portlet-header" )
+            .addClass( "ui-widget-header ui-corner-all" )
+            .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+        $( ".portlet-toggle" ).click(function() {
+            var icon = $( this );
+            icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+            icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+        });
+    });
 </script>
 
 <?php require('views/footer.php'); ?>
